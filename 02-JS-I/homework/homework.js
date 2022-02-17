a4bea93746eb186fff52c570fce123abec10710f
Aquiles Igarzabal <aqui.igarzabal@gmail.com>
2022-02-17 11:27:11 -0300

    retoque

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -179,7 +179,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  newstr= str +"!":
+  newstr= str +"!";
   return newstr;
 }
 
@@ -233,27 +233,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length != 1){
-    return "Dato incorrecto";
+  if(letra.length > 1){
+    return "Dato incorrecto"
   }
-  if (letra == "a"){
-    return "Es vocal";
-  }  
-  if (letra == "e"){
-    return "Es vocal";
-  }   
-  if (letra == "i"){
-    return "Es vocal";
-  }  
-  if (letra == "o"){
-    return "Es vocal";
-  }  
-  if (letra == "u"){
-    return "Es vocal";      
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
   }
-  
-  return "Dato incorrecto";
-  
+  return "Dato incorrecto"
+
 }
 
 
